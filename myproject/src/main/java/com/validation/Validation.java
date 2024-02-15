2ec0b8d5a0d1e926172f09b3f577c8cb48f52562
Tanmay-Kayande <tanmaykayande85@gmail.com>
2024-02-15 17:15:53 +0530

    feat: add certificates

--- myproject/src/main/java/com/validation/Validation.java
+++ myproject/src/main/java/com/validation/Validation.java
@@ -8,94 +8,75 @@ public class Validation {
 
 	private String result;
 	private static final long max_size = 20 * 1024 * 1024;
-	
-	
 
 	public String validateLoginData(String username, String password) {
-		
+
 		int usernameLen = username.length();
 		int passwordLen = password.length();
-		
-		if(usernameLen<5 || usernameLen>30) {
-			
+
+		if (usernameLen < 5 || usernameLen > 30) {
+
 			result = "invalid username";
-		}
-		else if (passwordLen<5 || passwordLen>30) {
-			
+		} else if (passwordLen < 5 || passwordLen > 30) {
+
 			result = "invalid password";
-		}
-		else {
+		} else {
 			result = "valid";
 		}
-		
+
 		return result;
 	}
 
-
-
-
 	public String validateMessage(String name, String email, String message) {
-		
+
 		int nameLen = name.length();
 		int emailLen = email.length();
 		int messageLen = message.length();
-		
-		if(nameLen<2 || nameLen>45) {
+
+		if (nameLen < 2 || nameLen > 45) {
 			result = "Invalid name";
-		}
-		else if(emailLen<10 || emailLen>45) {
+		} else if (emailLen < 10 || emailLen > 45) {
 			result = "Invalid email";
-		}
-		else if(messageLen<10 || messageLen>1000) {
+		} else if (messageLen < 10 || messageLen > 1000) {
 			result = "Message between 10 to 1000 char";
-		}
-		else {
+		} else {
 			result = "valid";
 		}
 		return result;
 	}
-
-
-
 
 	public String validateproject(ArrayList<Object> al) {
-		
+
 		String name = (String) al.get(0);
-		Part part = (Part)al.get(1);
+		Part part = (Part) al.get(1);
 		long size = part.getSize();
-		
-		if(!name.endsWith(".jpg")) {
-			
+
+		if (!name.endsWith(".jpg")) {
+
 			result = "invalid";
-		}		
-		else if(size> max_size) {
-			
+		} else if (size > max_size) {
+
 			result = "invalid";
-		}else {
-			
+		} else {
+
 			result = "valid";
 		}
-		
+
 		return result;
-		
+
 	}
 
-
-
-
 	public String validateUpdateProject(ArrayList<Object> all) {
-		
-		Part part = (Part)all.get(1);
+
+		Part part = (Part) all.get(1);
 		long size = part.getSize();
-		String name = (String)all.get(3);
-		
-		if(!name.endsWith(".jpg")) {
+		String name = (String) all.get(3);
+
+		if (!name.endsWith(".jpg")) {
 			result = "invalid";
-		}
-		else if(size>max_size) {
+		} else if (size > max_size) {
 			result = "invalid";
-		}
-		else {
+		} else {
 			result = "valid";
 		}
 		return result;
@@ -103,22 +84,41 @@ public class Validation {
 
 	public String validateEdu(ArrayList<Object> al) {
 		String institution = (String) al.get(1);
-        String degree = (String) al.get(2);
-        String desc = (String) al.get(4);
+		String degree = (String) al.get(2);
+		String desc = (String) al.get(4);
 
-        if (institution.length() > 200 || institution.length() <= 0) {
-            result = "invalid";
-        } else if (degree.length() > 100 || degree.length() <= 0) {
-            result = "invalid";
-        } else if (desc.length() > 1000 || desc.length() < 10) {
-            result = "invalid";
-        } else {
-            result = "valid";
-        }
+		if (institution.length() > 200 || institution.length() <= 0) {
+			result = "invalid";
+		} else if (degree.length() > 100 || degree.length() <= 0) {
+			result = "invalid";
+		} else if (desc.length() > 1000 || desc.length() < 10) {
+			result = "invalid";
+		} else {
+			result = "valid";
+		}
 
-        return result;
+		return result;
+	}
+	public String validateCer(ArrayList<Object> al) {
+	String certificate = (String) al.get(0);
+	String institution = (String) al.get(1);
+	String desc = (String) al.get(3);
+
+	if(certificate.length()>200||certificate.length()<=0)
+	{
+		result = "invalid";
+	}else if(institution.length()>100||institution.length()<=0)
+	{
+		result = "invalid";
+	}else if(desc.length()>1000||desc.length()<10)
+	{
+		result = "invalid";
+	}else
+	{
+		result = "valid";
 	}
 
-
+	return result;
+}
 
 }
--- myproject/src/main/webapp/admin.jsp
+++ myproject/src/main/webapp/admin.jsp
@@ -298,6 +298,26 @@ a {
 			</div>
 			
 			
+			<div class="col-lg-4 col-md-6 col-12 mt-4 pt-2">
+				<div class="card border-0 bg-light rounded shadow">
+					<div class="card-body p-4">
+					
+						<span class="badge rounded-pill bg-primary float-md-end mb-3 mb-sm-0"> Certificates	</span>
+						<h5>Certificates</h5>
+						<div class="mt-3">
+							<span class="text-muted d-block"><i class="fa fa-home"
+								aria-hidden="true"></i> <a href="#" target="_blank"
+								class="text-muted">TANMAY</a></span> <span class="text-muted d-block"><i
+								class="fa fa-map-marker" aria-hidden="true"></i> INDIA</span>
+						</div>
+						<div class="mt-3">
+							<a href="certificates.jsp" class="btn btn-primary">Edit Certificates</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			
 
 
 			<div style="text-align: center; padding-top: 50px;" class="mt-3">
